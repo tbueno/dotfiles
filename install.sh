3657a2ea6676525cdac0c8e681e490c60ccb9166
tbueno <tbueno@tbueno.com>
2019-10-01 13:51:46 -0300

    Replace manually handled terraform for tfenv

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo ">>>>>>>>>>> INSTALLING HOMEBREW <<<<<<<<<<<<<<<"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install zsh git gpg-agent wget go nvm rbenv zsh-syntax-highlighting
+brew install zsh git gpg-agent wget go nvm rbenv zsh-syntax-highlighting tfenv
 
 
 echo ">>>>>>>>>>> SYMLINKING DOTFILES <<<<<<<<<<<<<<<"
