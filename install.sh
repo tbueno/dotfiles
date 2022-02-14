165560a69e3fd5f637d0f9061b4061f2ed606574
tbueno <tbueno@tbueno.com>
2022-02-14 16:13:42 -0300

    Activate brew when new session starts
    Also, removes terraform

--- install.sh
+++ install.sh
@@ -2,9 +2,17 @@
 
 echo ">>>>>>>>>>> INSTALLING HOMEBREW <<<<<<<<<<<<<<<"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew install zsh git wget go zsh-syntax-highlighting asdf
 
+
+asdf plugin add golang
+asdf install golang latest
+asdf plugin add ruby
+asdf install ruby latest
+asdf plugin add python
+asdf install python latest
 
 echo ">>>>>>>>>>> SYMLINKING DOTFILES <<<<<<<<<<<<<<<"
 
@@ -15,11 +23,3 @@ ln -fs "$(pwd)/zsh" ~/.zsh
 ln -fs "$(pwd)/psqlrc" ~/.psqlrc
 ln -fs "$(pwd)/tool-versions" ~/.tool-versions
 
-asdf plugin add golang
-asdf install golang latest
-asdf plugin add ruby
-asdf install ruby latest
-asdf plugin add python
-asdf install python latest
-asdf pluging add terraform
-asdf install terraform latest
