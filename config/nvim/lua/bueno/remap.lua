cdd957b162917b7ed8506820e1b50a382c313478
tbueno <tbueno@gmail.com>
2023-05-30 19:15:46 -0300

    Add lazy.vim for plugin management

--- config/nvim/lua/bueno/remap.lua
+++
@@ -1,3 +0,0 @@
-vim.g.mapleader = " "
--- Open Explorer
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- config/nvim/lua/bueno/set.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.swapfile = false
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
