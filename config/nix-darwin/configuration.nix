5018264636be1fb6d61cba85c9844b7188a22990
Thiago Bueno <thiago.bueno@optibus.com>
2025-01-21 18:42:44 -0300

    Extract homebrew programs

--- config/nix-darwin/configuration.nix
+++ config/nix-darwin/configuration.nix
@@ -4,6 +4,7 @@
   nix.settings.experimental-features = "nix-command flakes";
 
   imports = [
+    ./system/homebrew.nix
     ./system/macos.nix
   ];
 
@@ -30,31 +31,6 @@
 
   programs.direnv.enable = true;
   programs.zsh.enable = true;
-
-  homebrew = {
-    enable = true;
-    onActivation = {
-      autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
-      upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
-      # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
-      cleanup = "zap";
-    };
-
-    brews = [
-      "pure"
-    ];
-
-    casks = [
-      "1password"
-      "arc"
-      "dropbox"
-      "firefox"
-      "iterm2"
-      "obsidian"
-
-      "visual-studio-code"
-    ];
-  };
 
   system.stateVersion = 4;
   nixpkgs.hostPlatform = "aarch64-darwin";
---
+++ config/nix-darwin/system/homebrew.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, user, home, usersPath, ... }:
+
+{
+  homebrew = {
+      enable = true;
+      onActivation = {
+        autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
+        upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
+        # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
+        cleanup = "zap";
+      };
+
+      brews = [
+        "pure"
+      ];
+
+      casks = [
+        "1password"
+        "arc"
+        "chatgpt"
+        "dropbox"
+        "firefox"
+        "iterm2"
+        "obsidian"
+        "raycast"
+        "visual-studio-code"
+      ];
+    };
+}
