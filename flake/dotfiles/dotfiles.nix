fd660c042a7edea81d484901b12c2b864a92fb9d
Thiago Bueno <tbueno@gmail.com>
2026-01-01 22:48:37 -0300

    Improve wezterm config

--- flake/dotfiles/dotfiles.nix
+++ flake/dotfiles/dotfiles.nix
@@ -4,5 +4,4 @@
   home.file.".psqlrc".source = ./psqlrc;
   home.file.".gitconfig".source = ./gitconfig;
   home.file.".gitignore_global".source = ./gitignore;
-  home.file.".wezterm.lua".source = ./wezterm.lua;
 }
