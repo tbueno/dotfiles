b2f51edf8910cb5bbe6e22fdf8950185dfa643ac
Thiago Bueno <tbueno@gmail.com>
2026-01-01 22:54:52 -0300

    Streamline configurations with camp

--- flake/home.nix
+++ flake/home.nix
@@ -13,20 +13,5 @@
     sessionPath = [
       "$HOME/dev/dotfiles/bin"
     ];
-    packages = with pkgs; [
-      bat
-      clojure
-      colima
-      devbox
-      docker
-      git
-      go
-      home-manager
-      jetbrains-mono
-      neovim
-      nodejs
-      zsh-syntax-highlighting
-      zsh-autosuggestions
-    ];
   };
 }
