0144dbc3f4dd5cc3d101a1c304c54c491b84371a
Thiago Bueno <tbueno@gmail.com>
2026-09-22 13:01:33 -0300

    Move wezterm files to home-manager
    Making it so that the autoreload works out of the box. Before I needed a
    full env reload in order to apply changes.

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,58 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.front_end = "WebGpu"
+
+config.default_cwd = wezterm.home_dir .. "/dev"
+
+config.scrollback_lines = 5000
+
+-- LOOK AND FEEL
+config.color_scheme = "catppuccin-macchiato"
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 16
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+
+-- Remove the title bar from the windown, since it is quite useles
+-- 'RESIZE' here means that the title bar is gone, but I can still resize
+-- the window with the mouse.
+
+config.window_decorations = "RESIZE"
+
+-- - Tab bar styling
+config.window_frame = {
+	font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
+}
+
+-- KEY BINDINGS
+
+config.disable_default_key_bindings = true
+config.keys = {
+	{ key = "f", mods = "SUPER", action = act.Search({ CaseSensitiveString = "" }) },
+	{ key = "\\", mods = "CTRL", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "CTRL", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "+", mods = "SUPER|CTRL", action = act.IncreaseFontSize },
+	{ key = "-", mods = "SUPER|CTRL", action = act.DecreaseFontSize },
+	{ key = "[", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(1) },
+	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "h", mods = "SUPER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "SUPER", action = act.ActivatePaneDirection("Down") },
+	{ key = "l", mods = "SUPER", action = act.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "SUPER", action = act.ActivatePaneDirection("Up") },
+	{ key = "r", mods = "SUPER", action = act.ReloadConfiguration },
+	{ key = "t", mods = "SUPER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "w", mods = "SUPER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
+	{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+}
+
+-- return the configuration to wezterm
+return config
--- flake/programs/wezterm.nix
+++ flake/programs/wezterm.nix
@@ -1,10 +1,14 @@
 { config, lib, pkgs, ... }:
 
+let
+  dotfilesDir = "${config.home.homeDirectory}/dev/dotfiles";
+in
 {
   home = {
     packages = with pkgs; [
       wezterm
     ];
-    file.".wezterm.lua".source = ../dotfiles/wezterm.lua;
+    file.".wezterm.lua".source =
+      config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/wezterm/wezterm.lua";
   };
 }
